43de9fb1a93849af2a32619bf6cc67137d004d2c
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-24 22:13:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,16 +10,6 @@ def map(array)
   result
 end
 
-# def map_to_negativize(source_array)
-# i = 0
-# new_array = []
-# while i < source_array.length
-# new_array << source_array[i] * -1
-#   i += 1
-# end
-# new_array
-# end 
-
 def reduce(array, sv=nil)
   if sv
     sum = sv
@@ -35,13 +25,3 @@ end
 sum
 end
 
-
-# def reduce_to_total(source_array, starting_point=0)
-#   sum = starting_point
-#   i = 0
-#   while i < source_array.length do
-#     sum += source_array[i]
-#     i += 1
-#   end
-#   sum
-# end
