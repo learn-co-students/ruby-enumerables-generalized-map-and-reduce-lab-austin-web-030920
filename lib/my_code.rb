6c7a05861c044360ad86d12b81d4608e2a0b940b
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-24 20:31:59 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
-# Your Code Here
+require 'pry' #binding.pry
+
+def map(array)
+  result = []
+  i = 0 
+  while i < array.length
+    result << yield(array[i])
+    i += 1 
+  end 
+  result
+end
+
+# def map_to_negativize(source_array)
+# i = 0
+# new_array = []
+# while i < source_array.length
+# new_array << source_array[i] * -1
+#   i += 1
+# end
+# new_array
+# end 
+
+# def reduce(array, starting_value=0)
+  
+  
+end
+
+
+# def reduce_to_total(source_array, starting_point=0)
+#   sum = starting_point
+#   i = 0
+#   while i < source_array.length do
+#     sum += source_array[i]
+#     i += 1
+#   end
+#   sum
+# end
