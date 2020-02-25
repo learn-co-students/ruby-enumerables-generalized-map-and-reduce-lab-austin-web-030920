2608b8b96c45c08c2733ceaf1c623b00f1fd088d
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-25 18:19:09 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,27 +1,84 @@
 require 'pry' #binding.pry
 
 def map(array)
-  result = []
   i = 0 
-  while i < array.length
-    result << yield(array[i])
-    i += 1 
-  end 
-  result
+  new = []
+    while i < array.length
+    new << yield(array[i])
+    i += 1
+    end
+    new
 end
 
+
 def reduce(array, sv=nil)
-  if sv
-    sum = sv
+  i = 0
+  if sv 
+    ttl = sv
     i = 0 
   else 
-    sum = array[0]
+    ttl = array[0]
     i = 1 
   end
-  while i < array.length
-    sum = yield(sum, array[i])
+    while i < array.length
+    ttl = yield(ttl, array[i])
     i += 1
-end
-sum
-end
+  end
+  ttl
+end 
+
+# reduce([1,2,3]1) do |x, y|
+# x + y 
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def map(array)
+#   result = []
+#   i = 0 
+#   while i < array.length
+#     result << yield(array[i])
+#     i += 1 
+#   end 
+#   result
+# end
+
+# def reduce(array, sv=nil)
+#   if sv
+#     sum = sv
+#     i = 0 
+#   else 
+#     sum = array[0]
+#     i = 1 
+#   end
+#   while i < array.length
+#     sum = yield(sum, array[i])
+#     i += 1
+# end
+# sum
+# end
 
