ed45b9a1974efccd6ae728eca6f02b8d11559a67
Dakota Willingham <kota@vaga.tk>
2020-02-27 20:36:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source_array)
+  counter = 0
+  result = []
+  while counter < source_array.length do
+    result << yield(source_array[counter])
+    counter += 1
+  end
+result
+end
+
+def reduce(source_array, sv = nil)
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum = source_array[0]
+    i = 1
+  end
+  while i < source_array.length do
+    sum = yield(sum, source_array[i])
+    i += 1
+  end
+  sum
+end
