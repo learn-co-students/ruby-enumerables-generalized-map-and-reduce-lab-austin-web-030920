fd59dab5761f8b698668ba20562a648a731073f9
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 03:23:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+
+def map(array)
+  numbers = []
+  i = 0
+  while i < array.length
+    numbers.push(yield(array[i]))
+    i += 1
+  end
+  return numbers
+end
+
+
+def reduce(array, opt = nil)
+ 
+  if opt
+    total = opt
+    i = 0
+  else
+    total = array[0]
+    i = 1
+  end
+  
+  while i < array.length
+    total = yield(total, array[i])
+    i += 1
+  end
+  
+  return total
+end
