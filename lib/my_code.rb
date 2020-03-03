eadc96ae62f537d4b074c0a2a29e24958838795e
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-03-03 03:00:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+
+def map(source)
+  result = []
+  i = 0
+  while i < source.length do
+    result << yield(source[i])
+    i += 1
+  end
+  result
+end
+
+
+def reduce(source, starting_point = nil)
+  if (source[0].class == Integer)
+    result = starting_point.to_i
+  else
+    result = !!starting_point
+  end
+  i = 0
+  while i < source.length do
+    result = yield(result, source[i])
+    i += 1
+  end
+  result
+end
