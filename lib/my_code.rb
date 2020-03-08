ce99336ddc3e20db7010e1971c63699ccc2c8b2a
github username <github email address>
2020-03-07 19:48:12 -0600

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+
+def map(array)
+    new = []
+    i = 0
+    while i < array.length do
+        new << yield(array[i])
+        i += 1
+    end
+
+    return new
+end
+
+def reduce(array, start=nil)
+
+    if start
+        acc = start
+        i = 0
+    else
+        acc = array[0]
+        i = 1
+    end
+    while i < array.length do
+        acc = yield(acc, array[i])
+
+        i += 1
+    end
+    
+    return acc
+end
